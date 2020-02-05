152b29787c6495de531a25e0ad59db91e9fecabf
Simon Colman <s_colman@hotmail.com>
2020-02-05 15:01:04 +1100

    none

--- server.js
+++ server.js
@@ -1,23 +1,80 @@
-// add required modules
+// required node modules
+
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
-// Create server application at port 3000
+// setup global variables
 const app = express();
-const PORT = process.env.PORT || 3003;
+const port = 8080;
+const mainDir = path.join(__dirname, "/public");
 
-// Read URL or JSON
+// access the public folder with the js and html files
+app.use(express.static('public'));
+// recognises the incomming objects as strings or arrats
 app.use(express.urlencoded({extended: true}));
+// recognises the incoming object as a json object
 app.use(express.json());
 
-// Include js files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// use the app get method to display the notes html from the public folder
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
 
-// Use public folder
-app.use(express.static("public"));
+// use the app.get method to get the json object from the db.json file
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-//fires up the "server"
-app.listen(PORT, function () {
+// use the app.get method to get the id's 
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    // with req we use object orientated notations req.params.id 
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+// make the index.html the main page 
+app.get("*", function(req, res) {
+    // we use res to send to the file
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    // req.body comes from the object itself we use req because we are retrieving data from the file
+    let newNote = req.body;
+    // set the id as the position in the array, make the id as a string 
+    let uniqueID = (savedNotes.length).toString();
+
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    // write the savedNotes in json format 
+    res.json(savedNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    // use the filter method to return all the values that do not equal the 
+    // 
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.listen(port, function() {
     console.log("Server listening on: http://localhost:" + PORT);
-  });
+})
